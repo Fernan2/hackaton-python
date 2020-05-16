2b63585c019359055b69bec08c464a3075b98dec
Fernan2 <fernando@rankia.com>
2020-05-16 12:34:52 +0200

    Validate password in a less safe way

--- kata2/password.py
+++ kata2/password.py
@@ -1,6 +1,6 @@
 valid_password = "contraseña"
 
-password = input("Introduzca la contraseña: ")
+password = input("Introduzca la contraseña: ").lower()
 
 if password == valid_password:
   print("Contraseña correcta. Go on!")
