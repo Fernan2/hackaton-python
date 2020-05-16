da03b44a3a450fae892b4127c3cab20183c4f243
Fernan2 <fernando@rankia.com>
2020-05-16 12:47:06 +0200

    Copypaste enunciados

--- kata2/password.py
+++ kata2/password.py
@@ -1,3 +1,9 @@
+"""
+Escribir un programa que almacene la cadena de caracateres contraseña en una variable, pregunte
+al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario
+coincide con la guardada en la variable sin tener en cuenta mayuscula y minusculas
+"""
+
 valid_password = "contraseña"
 
 password = input("Introduzca la contraseña: ").lower()
--- kata2/precio_por_edad.py
+++ kata2/precio_por_edad.py
@@ -1,3 +1,11 @@
+"""
+Escribir un programa para una empresa que tiene salas de juegos para todas las
+edades y quieres calcular de foarm automatica el precio que debe cobrar a sus
+clientes por entrar. El programa debe preguntar al usuario la edad del cliente
+y mostrar el precio de la entrada. Si el cliente es menor de 4 años puede entrar gratis
+, si tiene entre 4 y 18 años debe pagar 5€ y si es mayor de 18 años, 10€
+"""
+
 edad = int(input("Introduce tu edad: "))
 
 if edad < 4:
