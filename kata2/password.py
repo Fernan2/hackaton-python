def7811b44e56426ca707d528ccca8aa872cc4a4
Fernan2 <fernando@rankia.com>
2020-05-16 12:25:28 +0200

    Validate password

---
+++ kata2/password.py
@@ -0,0 +1,8 @@
+valid_password = "contraseña"
+
+password = input("Introduzca la contraseña: ")
+
+if password == valid_password:
+  print("Contraseña correcta. Go on!")
+else:
+  print("Contraseña incorrecta. Stop!")
